fed8549978a4404787882b990c92ba3af038b2f3
govl0407 <govl0407@gmail.com>
2024-05-26 01:21:13 +0900

    Initial commit

---
+++ src/main/java/com/mysite/pybo/PyboApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.pybo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/pybo/PyboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.pybo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
