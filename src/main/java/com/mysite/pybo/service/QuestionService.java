5aa2690c810dbaea6edf1f9e586c9e87a6d1ce94
govl0407 <govl0407@gmail.com>
2024-05-28 00:27:47 +0900

    add create_question

--- src/main/java/com/mysite/pybo/service/QuestionService.java
+++ src/main/java/com/mysite/pybo/service/QuestionService.java
@@ -1,11 +1,14 @@
 package com.mysite.pybo.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.mysite.pybo.domain.QA.Question;
 import com.mysite.pybo.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.mysite.pybo.service.DataNotFoundException;
+import java.time.LocalDateTime;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -26,4 +29,13 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
     }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
+    }
+
 }
